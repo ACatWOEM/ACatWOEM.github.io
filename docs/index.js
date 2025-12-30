0b00b712319a17eb63becd35b23918f4ae952112
Richard Farmer <157524994+ACatWOEM@users.noreply.github.com>
2025-12-29 19:45:27 -0800

    we do a bit of trolling

--- docs/index.js
+++ docs/index.js
@@ -1,11 +1,17 @@
+console.log('script.js loaded');
 const audio = document.getElementById('audio');
 const muteButton = document.getElementById('muteButton');
 const muteStatus = document.getElementById('muteStatus');
 const muted = './images/muted.png';
 const unmuted = './images/unmuted.png';
+var rickButton = document.getElementById('rickroll');
+var rickImage = document.getElementById('rickImage')
+const video = document.getElementById('code');
 let isMuted = true;
+let rolled = false;
+let trollCount = 0;
 
-function toggleMute(){
+function toggleMuteAudio(){
     audio.muted = !audio.muted;
 }
 function changeStatus(){
@@ -18,5 +24,43 @@ function changeStatus(){
     }
     isMuted = !isMuted;
 }
-
+function rickroll(){
+    if(trollCount == 0){
+        video.src = './video/nggyu.mp4';
+        video.play;
+        rolled = true;
+        rickButton.innerText = 'Mute?';
+    } if(trollCount == 1){
+        rickButton.innerText = 'NEVER';
+        rickImage = './images/umad.png';
+    } if(trollCount == 2){
+        rickButton.innerText = 'GONNA';
+    }if(trollCount == 3){
+        rickButton.innerText = 'GIVE';
+    }if(trollCount == 4){
+        rickButton.innerText = 'YOU';
+    }if(trollCount == 5){
+        rickButton.innerText = 'UP';
+    }if(trollCount == 6){
+        rickButton.innerText = 'Okay...';
+        video.src = './video/67.mp4';
+        video.play;
+    }if(trollCount == 7){
+        rickButton.innerText = 'I\'M KIDDING!!!';
+        video.src = './video/nggyu.mp4';
+    }if(trollCount == 8){
+        rickButton.innerText = 'fine...';
+    }if(trollCount ==9){
+        rickButton.innerText = 'muted';
+        video.muted = !video.muted;
+    }if (trollCount >= 10){
+        video.muted = !video.muted;
+        if(rickButton.innerText == 'muted'){
+            rickButton.innerText = 'mute';
+        }else{
+            rickButton.innerText='muted';
+        }
+    }
+    trollCount++;
+}
 muteButton.addEventListener('click', changeStatus);
